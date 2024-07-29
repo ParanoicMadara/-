92403cd9c02ae9b0595ab7470e1055c194d82530
ParanoicMadara <qqqaaawwwssseeedddrrr@mail.ru>
2024-07-29 11:38:17 +0300

    Add files via upload

---
+++ 2 zadanie.py
@@ -0,0 +1,5 @@
+dz_done = 12
+dz_ur = 1.5
+dz_name = 'Python,'
+dz_progress = dz_ur/dz_done
+print('Курс:',dz_name,'всего задач',dz_done,' затрачено часов:',dz_ur,'среднее время выполнения',dz_progress,'часа.')
